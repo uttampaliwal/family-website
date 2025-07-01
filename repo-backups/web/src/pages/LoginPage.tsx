d95fef41a20f890cc482a1e69b69be76cf2cabf9
Uttam Paliwal <uttam232002@gmail.com>
2025-07-01 07:33:10 +0530

    backup

---
+++ repo-backups/web/src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+function LoginPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">Login Page</h1>
+    </div>
+  );
+}
+
+export default LoginPage;
