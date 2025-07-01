dc8b9a75a3f714b8552df967835033f97c3fa284
Uttam Paliwal <uttam232002@gmail.com>
2025-07-01 14:38:58 +0530

    Delete repo-backups directory

--- repo-backups/web/src/pages/AboutPage.tsx
+++
@@ -1,9 +0,0 @@
-function AboutPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800">About Page</h1>
-    </div>
-  );
-}
-
-export default AboutPage;
--- repo-backups/web/src/pages/SignupPage.tsx
+++
@@ -1,9 +0,0 @@
-function SignupPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800">Signup Page</h1>
-    </div>
-  );
-}
-
-export default SignupPage;
