c32d16c4f54e727305817f75ca1155668566ff7a
Uttam Paliwal <uttam232002@gmail.com>
2025-07-04 00:23:11 +0530

    some fixes

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -9,12 +9,12 @@ function App() {
   return (
     <Router>
       {/* Permanent Logo at Top-Left of the Webpage */}
-      <Link to="/" className="absolute top-0 left-0 z-10 p-4"> {/* Removed debugging colors */}
-        <img src="/family-logo.webp" className="w-[80px] h-[80px] object-contain" alt="Family Logo" />
+      <Link to="/" className="absolute top-7 left-9 z-10"> {/* Adjusted top and left for slight shift */}
+        <img src="/family-logo.webp" className="w-[100px] h-[100px] object-contain" alt="Family Logo" />
       </Link>
 
       {/* Main Content Area */}
-      <div className="min-h-screen flex flex-col items-center w-full pt-[100px] pl-[100px]"> {/* Adjusted padding for logo */}
+      <div className="min-h-screen flex flex-col items-center w-full pt-[140px] pl-[140px]"> {/* Padding remains sufficient */}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
