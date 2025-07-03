5ebf4b993701bd5aad2e6d1824eeb2658a63057d
Uttam Paliwal <uttam232002@gmail.com>
2025-07-04 00:14:26 +0530

    some fixes

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -8,19 +8,19 @@ import VerifyEmailPage from './pages/VerifyEmailPage';
 function App() {
   return (
     <Router>
-      <div className="flex flex-col items-center p-[10px] w-full">
-        <Link to="/">
-          <img src="/family-logo.webp" className="logo-glow w-[100px] h-[100px]" alt="Family Logo" />
-        </Link>
-        <div className="w-full">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/verify-email" element={<VerifyEmailPage />} />
-          </Routes>
-        </div>
-        
+      {/* Permanent Logo at Top-Left of the Webpage */}
+      <Link to="/" className="absolute top-0 left-0 z-10 p-4"> {/* Removed debugging colors */}
+        <img src="/family-logo.webp" className="w-[80px] h-[80px] object-contain" alt="Family Logo" />
+      </Link>
+
+      {/* Main Content Area */}
+      <div className="min-h-screen flex flex-col items-center w-full pt-[100px] pl-[100px]"> {/* Adjusted padding for logo */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/verify-email" element={<VerifyEmailPage />} />
+        </Routes>
       </div>
     </Router>
   );
