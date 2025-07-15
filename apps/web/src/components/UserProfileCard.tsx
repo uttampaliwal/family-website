cb9657959acdd5207f69417e0fcd425874601a17
Uttam Paliwal <uttam232002@gmail.com>
2025-07-15 09:59:17 +0530

    added User Profile Card component.

---
+++ apps/web/src/components/UserProfileCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface UserProfileCardProps {}
+
+const UserProfileCard: React.FC<UserProfileCardProps> = ({}) => {
+  return (
+    <div>
+      <h1>UserProfileCard</h1>
+    </div>
+  );
+};
+
+export default UserProfileCard;
